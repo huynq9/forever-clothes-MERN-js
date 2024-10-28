45f3feb03622334b67689cebf6d92f3e21dfba18
huynq9 <nguyenquanghuyx8@gmail.com>
2024-10-29 00:26:41 +0700

    create navigation, fix tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Collections from "./pages/Collections";
+import Collection from "./pages/Collection";
 import About from "./pages/About";
-import Contact from "./pages/";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
 import Login from "./pages/Login";
 import PlaceOder from "./pages/PlaceOder";
+import Navbar from "./components/Navbar";
+import Contact from "./pages/Contact";
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vh] md:px-[7vh] lg:px-[9vh]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collections />} />
+        <Route path="/collection" element={<Collection />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product/:productId" element={<Product />} />
