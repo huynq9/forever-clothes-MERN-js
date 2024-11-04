de5a1f9788e1e9397d9d56de33548f0bbdcf4334
huynq9 <nguyenquanghuyx8@gmail.com>
2024-11-04 21:34:38 +0700

    Cart Page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -16,7 +16,6 @@ const Product = () => {
       if (item._id === productId) {
         setProductData(item);
         setImage(item.image[0]);
-        console.log(item);
         return null;
       }
     });
