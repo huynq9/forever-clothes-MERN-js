3c22fd7c8bdd05164156a6dec017af9f4a2970fb
huynq9 <nguyenquanghuyx8@gmail.com>
2024-11-04 17:07:38 +0700

    Create Add to Cart Feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,13 @@ import Navbar from "./components/Navbar";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vh] md:px-[7vh] lg:px-[9vh]">
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
