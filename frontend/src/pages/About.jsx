7c0ff87b428505c452a13e4390548e34f4d24b41
huynq9 <nguyenquanghuyx8@gmail.com>
2025-04-28 23:24:51 +0700

    About page & Contact page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
+import Title from "../components/Title";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+        <div className="my-10 flex flex-col md:flex-row gap-16">
+          <img
+            className="w-full md:max-w-[450px]"
+            src={assets.about_img}
+            alt=""
+          />
+          <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+              aliquid adipisci minus repellat iure soluta unde. Vitae
+              dignissimos alias sint officiis!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              aperiam omnis debitis odit velit qui ipsa nobis, minima, harum id
+              eum quod quisquam! Est ratione officia
+            </p>
+            <b className="text-gray-800">Our Mission</b>
+            <p>
+              Our Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Voluptas minus reiciendis aliquid odio culpa? Est optio nihil quo
+              rerum laborum, praesentium asperiores vitae ipsam nulla odio dolor
+              exceptur
+            </p>
+          </div>
+        </div>
+        <div className="text-xl py-4">
+          <Title text1={"WHY"} text2={"CHOOSE"} />
+        </div>
+        <div className="flex flex-col md:flex-row text-sm mb-20">
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b className="text-gray-600">Quantity Assurance:</b>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consectetur sint vero eos provident, dolores commodi obcaecati
+              natus?
+            </p>
+          </div>
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b className="text-gray-600">Convenience:</b>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consectetur sint vero eos provident, dolores commodi obcaecati
+              natus?
+            </p>
+          </div>
+          <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+            <b className="text-gray-600">Exeptional Customer Service:</b>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consectetur sint vero eos provident, dolores commodi obcaecati
+              natus?
+            </p>
+          </div>
+        </div>
+        <NewsletterBox />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
+import Title from "../components/Title";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          src={assets.contact_img}
+          className="w-full md:max-w-[480px]"
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-sans text-xl text-gray-600">Our Strore</p>
+          <p className="text-gray-500">
+            Hong Do, Phu Do <br /> Tu Liem, Ha Noi
+          </p>
+          <p className="text-gray-500">
+            Tel: 032-908-JQKA <br />
+            Email: {"nguyenquanghuyx8@gmail.com"}
+          </p>
+          <p className="font-sans text-xl text-gray-600">Careers At Forever</p>
+          <p className="text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+        <NewsletterBox />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
